e8e04c463fbf188f9154624d5b5e1c1665d14378
Sumeet Patil <68642212+SumeetPatil03@users.noreply.github.com>
2020-08-16 14:31:54 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,6 @@
+print ("Program by Sumeet Patil")
+from math import pi
+r = float(input ("enter a raduis"))
+print ( "for a circle with" + str(r) + "of raduis")
+A = (pi*r*r)
+print("The area is :" + str(A))
